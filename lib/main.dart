bb870fbbe2bb53aa00db1d2e98be66f2da3f123b
yderfre78 <72670647+yderfre78@users.noreply.github.com>
2021-10-28 15:02:14 -0500

    my first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_delivery/src/pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Delivery App Flutter',
+      initialRoute: 'login',
+      routes: {
+        'login': (BuildContext context) => const LoginPage(),
+      },
+    );
+  }
+}
